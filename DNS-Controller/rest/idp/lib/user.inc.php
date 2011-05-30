742b2c8d5346af17f0a2d7145255b1bb7c2818b2
Kasun Herath <kasun@thinkcube.com>
2011-05-30 22:24:05 +0530

    IDP usermanagement converted to LDAP

--- DNS-Controller/rest/idp/lib/user.inc.php
+++ DNS-Controller/rest/idp/lib/user.inc.php
@@ -1,22 +1,22 @@
 <?php
 
-function add($username, $password) {
+function add($subdomain, $username, $lastname, $password) {
 	global $shell_path;
 	$retval = true;
 
 	if ($retval) {
-		exec("cd $shell_path && sudo ./usermgmt add $username $password", $output, $retval1);
+		exec("cd $shell_path && sudo ./usermgmt add $subdomain $username $lastname $password", $output, $retval1);
         $retval = !$retval1;
 	}
 	
 	return $retval;
 }
 
-function delete_user($username){
+function delete_user($subdomain, $username){
         global $shell_path;
         $retval = true;
         if ($retval) {
-            exec("cd $shell_path && sudo ./usermgmt delete $username", $output, $retval1);
+            exec("cd $shell_path && sudo ./usermgmt delete $subdomain $username", $output, $retval1);
             $retval = !$retval1;
         }
 
