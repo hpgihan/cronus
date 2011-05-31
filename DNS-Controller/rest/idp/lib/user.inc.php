33556865b01258cc2f91c5f49787bb6fd748d67d
chanux <chanux@chanux-work.(none)>
2011-05-31 19:36:48 +0530

    Adding users to mysql DB in parallel to LDAP.

--- DNS-Controller/rest/idp/lib/user.inc.php
+++ DNS-Controller/rest/idp/lib/user.inc.php
@@ -5,8 +5,10 @@ function add($subdomain, $username, $lastname, $password) {
 	$retval = true;
 
 	if ($retval) {
-		exec("cd $shell_path && sudo ./usermgmt add $subdomain $username $lastname $password", $output, $retval1);
-        $retval = !$retval1;
+		exec("cd $shell_path && sudo ./usermgmt add $username $password", $output, $retval1);
+		exec("cd $shell_path && sudo ./myusermgmt add $username $password", $output, $retval2);
+        	
+		$retval = ! [[ $retval1 && $retval2 ]];
 	}
 	
 	return $retval;
@@ -15,12 +17,14 @@ function add($subdomain, $username, $lastname, $password) {
 function delete_user($subdomain, $username){
         global $shell_path;
         $retval = true;
+
         if ($retval) {
-            exec("cd $shell_path && sudo ./usermgmt delete $subdomain $username", $output, $retval1);
-            $retval = !$retval1;
+            exec("cd $shell_path && sudo ./usermgmt delete $username", $output, $retval1);
+	    exec("cd $shell_path && sudo ./myusermgmt delete $username", $output, $retval2);
+
+            $retval = ! [[ $retval1 && $retval2 ]];
         }
 
         return $retval;
-
 }
 ?>
