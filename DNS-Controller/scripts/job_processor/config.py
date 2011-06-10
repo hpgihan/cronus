8c07cb279e6d4633e9213fbe429e5753cd77722b
Kasun Herath <kasun@thinkcube.com>
2011-06-10 17:25:15 +0530

    job processor taking wrong idp ip error fixed

--- DNS-Controller/scripts/job_processor/config.py
+++ DNS-Controller/scripts/job_processor/config.py
@@ -28,6 +28,6 @@ job_type_delete = 5
 
 controller_ip='XXCNTRLIPXX'
 
-job_finalyze_url = 'https://controller_ip/api/utils/action/job/update'
+job_finalyze_url = 'https://' + controller_ip + '/api/utils/action/job/update'
 
 api_key='ca899e52f5d93bb1cdb623970c894f4cd17eeb3ea752e48a74bbacdb9ef18c08'
